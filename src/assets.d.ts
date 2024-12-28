d5725036420f51a18fb1935606b1950951debeaf
sunub <bsc5672@gmail.com>
2024-12-28 10:44:10 +0900

    feat : gltf, bin 파일을 타입 오류 없이 사용하기 위해 타입 선언문 추가

---
+++ src/assets.d.ts
@@ -0,0 +1,14 @@
+declare module "*.glb" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.hdr" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.gltf" {
+  const content: string;
+  export default content;
+}
