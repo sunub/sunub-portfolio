ace20bcfb1e3772d95fd91d75e19bdddf961c98d
sunub <bsc5672@gmail.com>
2024-12-28 10:43:43 +0900

    fix : build 시 3d 오브젝트 관련 파일을 추적하지 못하던 문제 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/",
+  assetsInclude: ["**/*.glb", "**/*.hdr", "**/*.gltf", "**/*.bin"],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@widgets": path.resolve(__dirname, "./src/widgets"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+    },
+  },
+  publicDir: "public",
+  build: {
+    assetsDir: "assets",
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+      output: {
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
+        assetFileNames: (assetInfo) => {
+          const fileName = assetInfo.name ?? "unknown";
+          const extType = fileName.split(".").pop()?.toLowerCase() ?? "";
+
+          if (/glb|hdr|gltf|bin/i.test(extType)) {
+            return `assets/models/[name]-[hash][extname]`;
+          }
+
+          return `assets/[name]-[hash][extname]`;
+        },
+      },
+    },
+  },
+});
